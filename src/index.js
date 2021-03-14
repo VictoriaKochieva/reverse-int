041e1f92854156a49a77121d682af493d70a93fe
VictoriaKochieva <kochievavictoria@gmail.com>
2021-03-14 18:34:39 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+ n = Math.abs(n)
+  let number = n.toString() ;
+   return number.split('').reverse().join('');
+    
+    
+    
+  } 
+      
+
+
   
-}
+
